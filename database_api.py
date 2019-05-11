3bf82f515ba8341548116698eae2eeb0089d1664
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-05-11 23:35:25 +1000

    Developed book borrowing
    Changed how queries are done and overhauled book search to behave better. Book borrowing calendar event creation still needs to be implemented.

--- database_api.py
+++ database_api.py
@@ -20,19 +20,117 @@ class GoogleDatabaseAPI:
         # Create connection to database
         self.__connection = MySQLdb.connect(host, user, password, database)
 
-    def execute_parameterized_query(self, query):
+    def search_books(self, clause=None, parameters=None):
         """
-        Execute the specified query in the database and return the results
+        Search the books table with given WHERE clause and parameters and
+        returns the result.
 
         Args:
-            query (str): The query that will be executed on the database
-            parameters (:obj:`tuple` of :obj:`str`): The parameters of the
-                parameterized query
+            clause (:obj: `str`, optional): WHERE clause in SQL syntax that
+                the clause will use. Default is none.
+            parameters (:obj:`tuple` of :obj:`str`, optional):
+                The parameters of a parameterized query. Default is none.
 
         Returns:
             The results of the query as a list of tuples
 
         """
+        # Define query
+        if(clause is None):
+            query = "SELECT * FROM BOOKS"
+        else:
+            query = "SELECT * FROM Books WHERE " + clause
+        # Execute query and return result
+        with self.__connection.cursor() as cursor:
+            if(parameters is None):
+                cursor.execute(query)
+            else:
+                cursor.execute(query, parameters)
+            return cursor.fetchall()
+
+    def create_borrow_event(self, bookID, userID):
+        """
+        Create a borrow entry for a given book in database
+
+        Args:
+            bookID (str): ID of the book to create borrow entry for
+            userID (str): ID of user that will be borrowing the book
+
+        """
+        # Define insert statement
+        query = "INSERT INTO BookBorrow (LmsUserID, BookID, Status, BorrowedDate) \
+                 VALUES (%s, %s, \"borrowed\", CURDATE())"
+        parameters = (userID, bookID)
+        # Create borrowed entry in book borrow table
         with self.__connection.cursor() as cursor:
             cursor.execute(query, parameters)
-            return cursor.fetchall()
+        # Commit changes
+        self.__connection.commit()
+
+    def get_userID(user_name):
+        """
+        Queries and returns the ID of the user by their name
+
+        Args:
+            user_name (str): The username of the user
+
+        Returns:
+            str: ID of the given user
+
+        """
+        # Define query
+        query = "SELECT LmsUserID FROM LmsUser WHERE UserName = %s"
+        parameters = (user_name)
+        # Execute query and get result
+        with self.__connection.cursor() as cursor:
+            cursor.execute(query, parameters)
+            return cursor.fetchone()[0]
+
+    def check_book_exists(self, bookID):
+        """
+        Queries the database to check if the book with ID exists.
+
+        Args:
+            bookID (str): The ID of the book to check
+
+        Returns:
+            bool: True if book with ID exists, False otherwise.
+
+        """
+        # Define query
+        query = "SELECT * FROM Books WHERE BookID = %s"
+        parameters = (bookID)
+        # Execute query and get result
+        with self.__connection.cursor() as cursor:
+            cursor.execute(query, parameters)
+            result = cursor.fetchone()
+        # Return whether or not the book exists
+        if(result is None or result[0] == "borrowed"):
+            return False
+        else:
+            return True
+
+    def check_book_available(self, bookID):
+        """
+        Queries the database to check if the book is not currently borrowed.
+
+        Args:
+            bookID (str): The ID of the book to check
+
+        Returns:
+            bool: True if book is not borrowed, False if book is borrowed.
+
+        """
+        # Define query
+        query = "SELECT Status FROM BookBorrowed WHERE BookID = %s AND BookBorrowedID in \
+            (SELECT MAX(BookBorrowedID) FROM BookBorrowed GROUP BY BookID)"
+        parameters = (bookID)
+        # Execute query and get result
+        with self.__connection.cursor() as cursor:
+            cursor.execute(query, parameters)
+            result = cursor.fetchone()
+        # Return whether or not the book is currently borrowed
+        if(result is None or result[0] == "borrowed"):
+            return False
+        else:
+            return True
--- master_console.py
+++ master_console.py
@@ -1,5 +1,6 @@
 import socket
 import database_api
+import datetime
 
 
 class MasterConsole:
@@ -38,7 +39,7 @@ class MasterConsole:
             conn, addr = sock.accept()
             with conn:
                 # Continuously loop getting username from reception, handling
-                # the user then sending a logoff message
+                # the user then sending a logoff message once user logs off
                 while True:
                     print("Waiting for user to connect...")
                     user_name = conn.recv(4096).decode()
@@ -54,31 +55,35 @@ class MasterConsole:
             user_name (str): Username of the connected user.
 
         """
-        # Display menu
-        print("Hello %s" % user_name)
-        print("Select an option:")
-        print("1. Search a book")
-        print("2. Borrow a book")
-        print("3. Return a book")
-        print("0. Logout")
+        while True:
+            # Display menu
+            print("Hello %s" % user_name)
+            print("Select an option:")
+            print("1. Search a book")
+            print("2. Borrow a book")
+            print("3. Return a book")
+            print("0. Logout")
 
-        # Get option from user
-        opt = input("Select an option: ")
-
-        # Handle option from user
-        if(opt == "1"):
-            # Let user search for a book
-            self.search_books()
-        elif(opt == "2"):
-            # Let user borrow book
-            self.borrow_books()
-        elif(opt == "3"):
-            # Let user return book
-            self.return_books()
-        elif(opt == "0"):
-            return
-        else:
-            print("Invalid option.")
+            # Get option from user
+            opt = None
+            while(opt is None):
+                opt = input("Select an option: ")
+                # Handle option from user
+                if(opt == "1"):
+                    # Let user search for a book
+                    self.search_books()
+                elif(opt == "2"):
+                    # Let user borrow book
+                    self.borrow_books(user_name)
+                elif(opt == "3"):
+                    # Let user return book
+                    self.return_books()
+                elif(opt == "0"):
+                    # Logs off master pi console
+                    return
+                else:
+                    print("Invalid option.")
+                    opt = None
 
     def search_books(self):
         """
@@ -101,60 +106,111 @@ class MasterConsole:
         while(opt is None):
             opt = input("Select an option: ")
             if(opt == "1"):
+                # Have user enter title to search by
                 title = input("Enter partial or full book title: ")
-                query += "Title LIKE %%%s%%"
+                clause += "Title LIKE %%%s%%"
                 values = (title)
             elif(opt == "2"):
+                # Have user enter author to search by
                 author = input("Enter partial or full author name: ")
-                query += "Author LIKE %%%s%%"
+                clause += "Author LIKE %%%s%%"
                 values = (author)
             elif(opt == "3"):
-                date_range_low = input(
-                    "Enter publication date low range(yyyy-mm-dd): ")
-                date_range_high = input(
-                    "Enter publication date high range(yyyy-mm-dd): ")
-                query += "PublishedDate BETWEEN \
+                # Have user enter date range to search by
+                valid_input = False
+                while not valid_input:
+                    date_range_low = input(
+                        "Enter publication date low range(yyyy-mm-dd): ")
+                    if(valid_date(date_range_low)):
+                        valid_input = True
+                    else:
+                        print("Date is invalid.")
+                valid_input = False
+                while not valid_input:
+                    date_range_high = input(
+                        "Enter publication date high range(yyyy-mm-dd): ")
+                    if(not valid_date(date_range_high)):
+                        valid_input = True
+                    else:
+                        print("Date is invalid.")
+                clause += "PublishedDate BETWEEN \
                           CAST(%s AS DATE) AND \
                           CAST(%s AS DATE)"
                 values = (date_range_low, date_range_high)
             elif(opt == "4"):
+                # Have user enter book ID to search by
                 book_id = input("Enter ID of book: ")
-                query += "BookID = %s"
+                clause += "BookID = %s"
                 values = (book_id)
             else:
                 print("Invalid option.")
                 opt = None
 
         # Query the books database for all books that satisfy conditions
-        results = self.__gdb.execute_parameterized_query(query, values)
-        # Build formatting rules
-        id_width = max(max(len(x[0]) for x in results), len("ID"))
-        title_width = max(max(len(x[1]) for x in results), len("Title"))
-        author_width = max(max(len(x[2]) for x in results), len("Author"))
-        pub_date_width = len("Publish Date")
-        total_width = id_width+title_width+author_width+pub_date_width+3
-        # Display all options on screen
-        print("%s|%s|%s|%s" % ("ID".center(id_width),
-                               "Title".center(title_width),
-                               "Author".center(author_width),
-                               "Publish Date".center(pub_date_width)))
-        print('-'*total_width)
-        for book in results:
-            print("%s|%s|%s|%s" % (str(book[0]).rjust(id_width, '0'),
-                                   str(book[1]).ljust(title_width),
-                                   str(book[2]).ljust(author_width),
-                                   str(book[3]).center()))
+        results = self.__gdb.search_books(clause, values)
+        if(results is not None):
+            # Build formatting rules
+            id_width = max(max(len(x[0]) for x in results), len("ID"))
+            title_width = max(max(len(x[1]) for x in results), len("Title"))
+            author_width = max(max(len(x[2]) for x in results), len("Author"))
+            pub_date_width = len("Publish Date")
+            total_width = id_width+title_width+author_width+pub_date_width+3
+            # Display all options on screen
+            print("%s|%s|%s|%s" % ("ID".center(id_width),
+                                   "Title".center(title_width),
+                                   "Author".center(author_width),
+                                   "Publish Date".center(pub_date_width)))
+            print('-'*total_width)
+            for book in results:
+                print("%s|%s|%s|%s" % (str(book[0]).rjust(id_width),
+                                       str(book[1]).ljust(title_width),
+                                       str(book[2]).ljust(author_width),
+                                       str(book[3]).center()))
+        else:
+            print("No books were found with this filter.")
 
-    def borrow_books(self):
+    def borrow_books(self, user_name):
         """
         Asks user for the id of the book they wish to borrow,
         creates a borrow event in database and creates a
         calendar event for the borrowed book.
 
-        TODO
+        Args:
+            user_name (str): Username of the user borrowing the book
+
+        TODO: Add calendar event creation
 
         """
-        pass
+        # Ask the user for the ID of the book they would like to borrow
+        id = input("Enter the ID of book you would like to borrow: ")
+        # Check that a book with that ID exists
+        if(self.__gdb.check_book_exists(id)):
+            # Inform the user that the book does not exist
+            print("Book with given ID does not exist.")
+        else:
+            # Check if the book has been borrowed and not yet returned
+            if(self.__gdb.check_book_available(id)):
+                # Inform the user the book has already been borrowed
+                print("This book has already been borrowed.")
+            else:
+                # Get user id of user
+                userID = self.__gdb.get_userID(user_name)
+                # Insert borrow event into database
+                self.__gdb.create_borrow_event(userID)
+                # Create borrow event in calendar TODO
+                pass
+
+        # Ask the user if they would like to borrow another book
+        while True:
+            response = input("Would you like to borrow another book?(Y/N): ")
+            if(response.upper() == "Y"):
+                # Recursively calls borrow books again
+                self.borrow_books()
+                return
+            elif(response.upper() == "N"):
+                return
+            else:
+                print("Invalid option.")
 
     def return_books(self):
         """
@@ -166,6 +222,29 @@ class MasterConsole:
 
         """
         pass
+
+    @staticmethod
+    def valid_date(date)
+    """
+    Checks if given date is valid and returns true if so, false if not.
+
+    Args:
+        date (str): String of date in the format of YYYY-MM-DD.
+
+    Returns:
+        Whether or not the date is valid.
+
+    """
+    # Split date into year, month and day
+    year, month, day = date.split("-")
+    try:
+        # Attempt to parse the date
+        datetime.datetime(int(year), int(month), int(day))
+    except ValueError:
+        # Parse failed, so return false
+        return False
+    # Parse was successful, so return true
+    return True
 
 # Starts the Master Pi Console
 if __name__ == "__main__":
