b9cd676a893639f02bec845e8c6dabd5e6d0877f
James Lauri <james.lainewyllie@gmail.com>
2019-05-26 09:10:46 +1000

    Add basic template for api

--- admin_app/app/admin.py
+++ admin_app/app/admin.py
@@ -1,7 +1,17 @@
-from flask import Flask
-from config import Config
+from flask import Flask, Blueprint, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os, requests, json
 
-app = Flask(__name__)
-app.config.from_object(Config)
+site = Blueprint("site", __name__)
 
-from app import routes
+# Client webpage. # Landing Page of website
+
+@site.route("/")
+def index():
+    # Use REST API.
+    #Our API is focused on books
+    response = requests.get("http://127.0.0.1:5000/book")
+    data = json.loads(response.text)
+
+    return render_template("index.html", book = data)
