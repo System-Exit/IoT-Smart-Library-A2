4622353d74dfbeb617742fdf4e9d01609b69f581
nekosoft <yiannaparis@gmail.com>
2019-05-25 23:23:51 +1000

    added main

--- test_voice_ui.py
+++ test_voice_ui.py
@@ -1,7 +1,10 @@
-# import unittest
-# from voice_ui import VoiceRecognition
+import unittest
+from voice_ui import VoiceRecognition
 
-# class TestVoiceRecognition(unittest.TestCase):
+class TestVoiceRecognition(unittest.TestCase):
     
-#     def test_voice_search_book(self):
-#         self.assertEqual('test'.search_books(), '1|Test1|TestAuthor| 2019-05-14')
+    def test_voice_search_book(self):
+        self.assertEqual('test'.search_books(), '1|Test1|TestAuthor| 2019-05-14')
+
+if __name__ == '__main__':
+    unittest.main()
