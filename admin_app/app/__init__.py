d66bd3e26d8ddc12f2eef38bda22f4c2eaf87acf
James Lauri <james.lainewyllie@gmail.com>
2019-05-26 23:26:45 +1000

    Add login credentials to login page

--- admin_app/app/__init__.py
+++ admin_app/app/__init__.py
@@ -15,7 +15,7 @@ from app import routes
 @site.route("/")
 def index():
     
-    # response = requests.get("http://127.0.0.1:5000/book")
+    response = requests.get("http://127.0.0.1:5000/")
     # data = json.loads(response.text)
 
     #return render_template("index.html", book = data)
--- admin_app/app/routes.py
+++ admin_app/app/routes.py
@@ -2,13 +2,18 @@ from flask import render_template
 from app import app
 from app.forms import LoginForm
 
-#Contains all the routes for the application plit into methods
+#Contains all the routes for the application split into methods
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    error = None
+
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign in', form=form)
+        
+        if(request.form['username'] == "jaqen" and request.form['password'] == "hghar"):
+            return redirect('/index')
+        else:
+            error = "Invalid username or password"
+
+    return render_template('login.html', title='Sign in', form=form, error=error)
