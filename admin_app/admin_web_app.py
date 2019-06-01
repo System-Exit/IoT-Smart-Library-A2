32ca69d27e2bb2b61e04b8569385b3bfc761c2e8
James Lauri <james.lainewyllie@gmail.com>
2019-06-01 22:25:59 +1000

    Modify database URI path

--- admin_app/admin_web_app.py
+++ admin_app/admin_web_app.py
@@ -9,26 +9,41 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Update HOST and PASSWORD appropriately.
-HOST = "35.244.115.76"
-USER = "default"
-PASSWORD = "default"
-DATABASE = "Library"
+HOST = '35.244.115.76'
+USER = 'root'
+PASSWORD = 'root'
+DATABASE = 'Library'
 
-database_path = "mysql://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DATABASE)
+#URL to connect to:
+# mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
 
-app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+INSTANCE_NAME = 'coral-silicon-242307:australia-southeast1:masterpi'
 
+
+SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@{host}/{database}'
+    '?unix_socket=/cloudsql/{instance_name}').format(
+        user=USER, password=PASSWORD, host = HOST, 
+        database=DATABASE, instance_name=INSTANCE_NAME
+    )
+
+# database_path = "mysql+pymysql://{}:{}@/{}?unix_socket=/cloudsql/{}".format(USER, PASSWORD, DATABASE, INSTANCE_NAME)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 db.init_app(app)
 
 app.register_blueprint(api)
 #app.register_blueprint(site)
 
 # Database information for Daniels Cloud SQL
-#HOST = "35.244.106.216"
-#USER = "masterpi"
-#PASSWORD = "ipretsam"
-#DATABASE = "Library"
+# HOST = "35.244.106.216"
+# USER = "masterpi"
+# PASSWORD = "ipretsam"
+# DATABASE = "Library"
+# database_path = "mysql://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DATABASE)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
