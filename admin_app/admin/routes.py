95118c5b61bc1c17b636773fd6efb8a28936439e
James Lauri <james.lainewyllie@gmail.com>
2019-06-12 16:48:33 +1000

    Fill in delete route

--- admin_app/admin/routes.py
+++ admin_app/admin/routes.py
@@ -76,5 +76,11 @@ def add():
 
 @site.route('/delete')
 def delete():
-  
 
+  #Get Title of book to delete
+  bookID = request.form['bookID'] 
+  # Use REST API.
+  response = requests.delete("http://127.0.0.1:5000/api/book/" + bookID)
+  data = json.loads(response.text)
+
+  return redirect("/")
